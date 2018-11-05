aebb746917d21778312f3d6f44106d873ff7afb6
tomwey <tomwey@163.com>
2018-11-05 18:12:00 +0800

    ccc

--- src/pages/operation-history/operation-history.ts
+++ src/pages/operation-history/operation-history.ts
@@ -15,11 +15,52 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OperationHistoryPage {
 
+  error: any = null;
+  data: any = [];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.data = [
+      {
+        action: '新增实有人口',
+        address: '内江市中区安居街38号',
+        time: '2018-09-10 12:30',
+        type: 10,
+      },
+      {
+        action: '新增实有单位',
+        address: '内江市中区安居街25号',
+        time: '2018-08-20 12:30',
+        type: 20,
+      },
+      {
+        action: '新增从业人员',
+        address: '四川内江雅居实业有限公司',
+        time: '2018-05-20 12:30',
+        type: 30,
+      },
+      {
+        action: '房屋日常检查',
+        address: '内江市中区安居街25号',
+        time: '2018-05-20 12:30',
+        type: 40,
+      },
+      {
+        action: '房屋日常检查',
+        address: '内江市中区安居街25号',
+        time: '2018-05-20 12:30',
+        type: 40,
+      },
+      {
+        action: '单位日常检查',
+        address: '四川内江雅居实业有限公司',
+        time: '2018-05-20 12:30',
+        type: 50,
+      },
+    ];
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad OperationHistoryPage');
+    // console.log('ionViewDidLoad OperationHistoryPage');
   }
 
 }
